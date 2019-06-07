711853970deb0b2d7c4585bf4adc6a60bd6416de
Hiroyuki Nakayama <hnakayam@redhat.com>
2019-06-07 12:14:16 +0900

    Update setup_jenkins.sh/Jenkinsfile for homework

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -14,12 +14,42 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 
 # Set up Jenkins with sufficient resources
 # TBD
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param VOLUME_CAPACITY=2Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
 # TBD
+oc new-build --name=jenkins-agent-appdev --strategy=docker --dockerfile=- <<EOF
+  FROM quay.io/openshift/origin-jenkins-agent-maven:4.1.0
+  USER root
+  RUN curl https://copr.fedorainfracloud.org/coprs/alsadi/dumb-init/repo/epel-7/alsadi-dumb-init-epel-7.repo -o /etc/yum.repos.d/alsadi-dumb-init-epel-7.repo && \
+      curl https://raw.githubusercontent.com/cloudrouter/centos-repo/master/CentOS-Base.repo -o /etc/yum.repos.d/CentOS-Base.repo && \
+      curl http://mirror.centos.org/centos-7/7/os/x86_64/RPM-GPG-KEY-CentOS-7 -o /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+  RUN DISABLES="--disablerepo=rhel-server-extras --disablerepo=rhel-server --disablerepo=rhel-fast-datapath --disablerepo=rhel-server-optional --disablerepo=rhel-server-ose --disablerepo=rhel-server-rhscl" && \
+      yum \$DISABLES -y --setopt=tsflags=nodocs install skopeo && yum clean all
+  USER 1001
+EOF
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # TBD
+oc create -f - <<EOF
+kind: BuildConfig
+apiVersion: v1
+metadata:
+  name: tasks-pipeline
+spec:
+  source:
+    type: Git
+    git:
+      uri: $REPO
+    contextDir: openshift-tasks
+  strategy:
+    type: JenkinsPipeline
+    jenkinsPipelineStrategy:
+      env:
+      - name: GUID
+        value: $GUID
+      jenkinsfilePath: Jenkinsfile
+EOF
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
